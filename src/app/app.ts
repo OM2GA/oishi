f576a3aadb59f85bacc6ebfc59d7ed00d1e21e8c
Maxence <maxence.coste.mc@gmail.com>
2025-12-03 11:54:51 +0100

    footer modifs

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
+import { Footer } from './footer/footer'; // Import du Footer
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Header],
+  imports: [RouterOutlet, Header, Footer], // Ajout du Footer aux imports
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
