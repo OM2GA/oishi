824bb636a2ee9fd4f887139391c3274022c825a6
Kolyox <alikan2907@gmail.com>
2025-12-04 22:23:32 +0100

    finalisation du header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { Inscription } from './inscription/inscription';
+import { Statistiques } from './statistiques/statistiques';
+import { ListeBox } from './liste-box/liste-box';
 
 export const routes: Routes = [
+    { path: '', redirectTo: '/inscription', pathMatch: 'full' }, 
     { path: 'inscription', component: Inscription },
-    { path: '', redirectTo: '/inscription', pathMatch: 'full' }
+    { path: 'statistiques', component: Statistiques },
+    { path: 'liste-box', component: ListeBox },
 ];
