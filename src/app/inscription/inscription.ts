5b15e471c6b1bb1915cd24e20db8f646a26302b1
Samuel <samuelrabari56@gmail.com>
2025-12-04 12:24:38 +0100

    page inscription basique

--- src/app/inscription/inscription.ts
+++ src/app/inscription/inscription.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-inscription',
-  imports: [],
+  imports: [FormsModule, RouterLink],
   templateUrl: './inscription.html',
   styleUrl: './inscription.css',
 })
 export class Inscription {
+  formData = {
+    nom: '',
+    prenom: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  };
 
+  showPassword = false;
+
+  togglePassword(): void {
+    this.showPassword = !this.showPassword;
+  }
+
+  onSubmit(): void {
+    if (this.formData.password !== this.formData.confirmPassword) {
+      alert('Les mots de passe ne correspondent pas');
+      return;
+    }
+
+    console.log('Données du formulaire:', this.formData);
+    // Ici vous pouvez ajouter la logique d'inscription (appel API, etc.)
+  }
 }
