804c3ee1959e4303ddb973084172634eea303686
Kolyox <alikan2907@gmail.com>
2025-12-11 17:03:01 +0100

    ajustement taille d'une div

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { ListeBox } from './liste-box/liste-box';
 import { Connexion } from './connexion/connexion';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/inscription', pathMatch: 'full' },
+    { path: '', redirectTo: '/connexion', pathMatch: 'full' },
     { path: 'inscription', component: Inscription },
     { path: 'connexion', component: Connexion },
     { path: 'statistiques', component: Statistiques },
