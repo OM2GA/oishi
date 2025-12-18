c494387c83f68643f7391ba51d12a0408614bf18
Maxence <maxence.coste.mc@gmail.com>
2025-12-18 09:10:34 +0100

    liste-box chargement instant

--- src/app/liste-box/liste-box.ts
+++ src/app/liste-box/liste-box.ts
@@ -1,30 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Data } from '../data';
-import { Footer} from '../footer/footer';
+import { Footer } from '../footer/footer';
 import { Header } from '../header/header';
 import { RouterModule } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-liste-box',
-  imports: [CommonModule,Footer,Header,RouterModule],
+  imports: [CommonModule, Footer, Header, RouterModule],
   templateUrl: './liste-box.html',
   styleUrl: './liste-box.css',
 })
 export class ListeBox implements OnInit {
-boxes: any;
+  boxes$: Observable<any> | undefined;
 
-  constructor(private data: Data) {
-    this.data = data;
-   }
+  constructor(private data: Data) { }
 
   ngOnInit(): void {
-    this.getBoxes();
-  }
-
-  getBoxes() {
-    this.data.getBoxesAPI().subscribe((res: any) => {
-      this.boxes = res;
-    });
+    this.boxes$ = this.data.getBoxesAPI();
   }
 }
