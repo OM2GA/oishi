e2b354ddfc1cadfd49cdd65c7a114a7a8ad6cd9e
Maxence <maxence.coste.mc@gmail.com>
2026-01-05 16:43:52 +0100

    ajout d'un message d'erreur et blocage de l'ajout de boxes supplémentaires lorsque que 10 boxes ont été ajouté au panier dans list-box

--- src/app/composant/liste-box/liste-box.ts
+++ src/app/composant/liste-box/liste-box.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Data } from '../../service/data';
 import { Footer } from '../footer/footer';
@@ -15,18 +15,20 @@ import { Observable } from 'rxjs';
 export class ListeBox implements OnInit {
   boxes$: Observable<any> | undefined;
   prenom_utilisateur: string | null = '';
+  message: string = '';
 
-  constructor(private data: Data) { }
+
+  constructor(private data: Data, private cd: ChangeDetectorRef) { }
 
   ngOnInit(): void {
     this.boxes$ = this.data.getBoxesAPI();
-    //regler erreur localStorage
     if (typeof window !== 'undefined') {
       this.prenom_utilisateur = localStorage.getItem('user_prenom');
     }
   }
 
   addPanier(idBox: number): void {
+    this.message = '';
     const idClient = localStorage.getItem('user_id');
 
     if (!idClient) {
@@ -34,16 +36,31 @@ export class ListeBox implements OnInit {
       return;
     }
 
-    this.data.addToPanier(idBox).subscribe({
-      next: (res: any) => {
-        if (res.id_commande) {
-          localStorage.setItem('id_commande', res.id_commande);
+    this.data.getPanier().subscribe({
+      next: (panierRes: any) => {
+        let totalArticles = 0;
+        if (panierRes && panierRes.panier) {
+          totalArticles = panierRes.panier.reduce((acc: number, item: any) => acc + Number(item.quantite), 0);
         }
-        console.log('Ajout de la box réussie');
+
+        if (totalArticles >= 10) {
+          this.message = "Vous avez atteint la limite de 10 articles dans votre panier.";
+          this.cd.detectChanges();
+        } else {
+          this.data.addToPanier(idBox).subscribe({
+            next: (res: any) => {
+              if (res.id_commande) {
+                localStorage.setItem('id_commande', res.id_commande);
+              }
+              console.log('Ajout de la box réussie');
+            },
+            error: (err) => {
+              console.error('Erreur ajout panier', err);
+            }
+          });
+        }
       },
-      error: (err) => {
-        console.error('Erreur ajout panier', err);
-      }
+      error: (err) => console.error("Erreur vérification panier", err)
     });
   }
 }
